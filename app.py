5480a792ce6258b0302f61b9c2689ca2ad787944
Musa Adamu Wakili <musawakiliml@hotmail.com>
2023-08-09 10:53:38 +0100

    Update: Added new package

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from fastapi import FastAPI, Form, Response
+from fastapi import FastAPI, Form, Response, Request, HTTPException
 from twilio.twiml.messaging_response import MessagingResponse
+from twilio.request_validator import RequestValidator
+from pydotenv import 
 
 app = FastAPI()
 
 @app.post("/hook")
-async def chat(From: str = Form(...), Body: str = Form(...)):
+async def chat(request: Request, From: str = Form(...), Body: str = Form(...)):
+    
+    validator = RequestValidator
+    
     response = MessagingResponse()
     msg = response.message(f"Hi {From}, you said: {Body}")
     return Response(content=str(response), media_type="application/xml")
